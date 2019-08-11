8d46dfc982bfd4b26043bf773f282e36f1e8aba9
Dmitriy Movchan <dmitriy.movch@gmail.com>
2019-08-11 22:49:39 +0300

    fix

--- source/components/Scheduler/index.js
+++ source/components/Scheduler/index.js
@@ -8,231 +8,226 @@ import FlipMove from 'react-flip-move';
 import { api, MAIN_URL, TOKEN } from '../../REST'; // ! Импорт модуля API должен иметь именно такой вид (import { api } from '../../REST')
 
 export default class Scheduler extends Component {
-    state = {
-        isTasksFetching: false,
-        newTaskMessage: '',
-        tasksFilter: '',
-        tasks: [],
+  state = {
+    isTasksFetching: false,
+    newTaskMessage: '',
+    tasksFilter: '',
+    tasks: [],
+    allTasksChecked: false,
+    filteredTasks: [],
+  };
+
+  componentDidMount() {
+    this._fetchTasksAsync();
+  }
+
+  _updateTaskAsync = async updatedTask => {
+    this._setTasksFetchingState(true);
+
+    if (!updatedTask.completed && this.state.allTasksChecked) {
+      this.setState(() => ({
         allTasksChecked: false,
-        filteredTasks: [],
-    };
-
-    componentDidMount() {
-        this._fetchTasksAsync();
+      }));
     }
 
-    _updateTaskAsync = async updatedTask => {
-        this._setTasksFetchingState(true);
+    const [updatedTasks] = await api.updateTask(MAIN_URL, TOKEN, updatedTask);
 
-        if (updatedTask.completed === false && this.state.allTasksChecked !== false) {
-            this.setState(() => ({
-                allTasksChecked: false,
-            }));
-        }
+    this.setState(({ tasks }) => ({
+      tasks: this._sortTasks(
+        tasks.map(task => (task.id === updatedTasks.id ? updatedTasks : task))
+      ),
+    }));
 
-        let updatedTasks = await api.updateTask(MAIN_URL, TOKEN, updatedTask);
-        updatedTasks = updatedTasks[0];
+    this._setTasksFetchingState(false);
+  };
 
-        this.setState(({ tasks }) => ({
-            tasks: this._sortTasks(
-                tasks.map(task => (task.id === updatedTasks.id ? updatedTasks : task))
-            ),
-        }));
+  _removeTaskAsync = async taskId => {
+    this._setTasksFetchingState(true);
 
-        this._setTasksFetchingState(false);
-    };
+    const removedTask = await api.removeTask(MAIN_URL, TOKEN, taskId);
 
-    _removeTaskAsync = async taskId => {
-        this._setTasksFetchingState(true);
-
-        const removedTask = await api.removeTask(MAIN_URL, TOKEN, taskId);
-
-        if (removedTask) {
-            this.setState(({ tasks }) => {
-                const newTasks = tasks.filter(task => task.id !== taskId);
-                return { tasks: [...newTasks] };
-            });
-        }
-
-        this._setTasksFetchingState(false);
-    };
-
-    _createTaskAsync = async event => {
-        event.preventDefault();
-
-        if (this.state.newTaskMessage === '') {
-            return null;
-        }
-
-        this._setTasksFetchingState(true);
-
-        const createdTask = await api.createTask(MAIN_URL, TOKEN, this.state.newTaskMessage);
-
-        this.setState(({ tasks }) => {
-            return { tasks: [...tasks, createdTask], newTaskMessage: '' };
-        });
-
-        this._setTasksFetchingState(false);
-    };
-
-    _updateTasksFilter = event => {
-        this.setState({ tasksFilter: event.target.value.toLowerCase() });
-    };
-
-    _updateNewTaskMessage = event => {
-        this.setState({ newTaskMessage: event.target.value });
-    };
-
-    _completeAllTasksAsync = async () => {
-        let notCompletedTasks = this.state.tasks.filter(task => task.completed !== true);
-        if (notCompletedTasks.length === 0) {
-            return null;
-        }
-
-        this._setTasksFetchingState(true);
-
-        notCompletedTasks = notCompletedTasks.map(item => {
-            return { ...item, completed: true };
-        });
-
-        api.completeAllTasks(MAIN_URL, TOKEN, notCompletedTasks);
-
-        this.setState(state => {
-            const tasks = state.tasks.map(item => {
-                return { ...item, completed: true };
-            });
-
-            return { tasks: this._sortTasks(tasks), allTasksChecked: true };
-        });
-
-        this._setTasksFetchingState(false);
-    };
-
-    _getAllCompleted = () => {
-        if (this.state.tasks.some(task => task.completed === false) === false) {
-            return true;
-        }
-        return false;
-    };
-
-    _sortTasks = tasks => {
-        let tasksCopy = JSON.parse(JSON.stringify(tasks));
-
-        tasksCopy.sort((a, b) => {
-            if (a.favorite > b.favorite) {
-                return -1;
-            }
-            if (a.favorite < b.favorite) {
-                return 1;
-            }
-            return 0;
-        });
-
-        tasksCopy.sort((a, b) => {
-            if (a.completed < b.completed) {
-                return -1;
-            }
-            if (a.completed > b.completed) {
-                return 1;
-            }
-            return 0;
-        });
-
-        return tasksCopy;
-    };
-
-    _fetchTasksAsync = async () => {
-        this._setTasksFetchingState(true);
-
-        const tasks = await api.fetchTasks(MAIN_URL, TOKEN);
-
-        //console.log(tasks);
-
-        this.setState({
-            tasks: this._sortTasks(tasks),
-        });
-
-        if (this._getAllCompleted()) {
-            this.setState({
-                allTasksChecked: true,
-            });
-        }
-
-        this._setTasksFetchingState(false);
-    };
-
-    _setTasksFetchingState = value => {
-        this.setState({
-            isTasksFetching: value,
-        });
-    };
-
-    render() {
-        const { tasks, isTasksFetching, allTasksChecked, tasksFilter, newTaskMessage } = this.state;
-        let taskJSX;
-
-        const taskCallback = task => {
-            return (
-                <Catcher key={task.id}>
-                    <Task
-                        {...task}
-                        _removeTaskAsync={this._removeTaskAsync}
-                        _updateTaskAsync={this._updateTaskAsync}
-                    />
-                </Catcher>
-            );
-        };
-
-        if (tasksFilter === '') {
-            taskJSX = tasks.map(taskCallback);
-        } else {
-            let filteredTasks = tasks.filter(task => task.message.includes(tasksFilter));
-
-            taskJSX = filteredTasks.map(taskCallback);
-        }
-
-        return (
-            <section className={Styles.scheduler}>
-                <Spinner isSpinning={isTasksFetching} />
-                <main>
-                    <header>
-                        <h1>Todo List</h1>
-                        <input
-                            placeholder='Поиск'
-                            type='search'
-                            value={tasksFilter}
-                            onChange={this._updateTasksFilter}
-                        />
-                    </header>
-                    <section>
-                        <form onSubmit={this._createTaskAsync}>
-                            <input
-                                maxLength='50'
-                                placeholder='Описaние моей новой задачи'
-                                type='text'
-                                onChange={this._updateNewTaskMessage}
-                                value={newTaskMessage}
-                            />
-                            <button>Добавить задачу</button>
-                        </form>
-
-                        <ul>
-                            <FlipMove>{taskJSX}</FlipMove>
-                        </ul>
-                    </section>
-                    <footer>
-                        <Checkbox
-                            onClick={this._completeAllTasksAsync}
-                            className={Styles.toggleTaskCompletedState}
-                            color1='#363636'
-                            color2='#FFF'
-                            checked={allTasksChecked}
-                            hover={false}
-                        />
-                        <span className={Styles.completeAllTasks}>Все задачи выполнены</span>
-                    </footer>
-                </main>
-            </section>
-        );
+    if (removedTask) {
+      this.setState(({ tasks }) => {
+        return { tasks: [...tasks.filter(task => task.id !== taskId)] };
+      });
     }
+
+    this._setTasksFetchingState(false);
+  };
+
+  _createTaskAsync = async event => {
+    event.preventDefault();
+
+    if (this.state.newTaskMessage === '') {
+      return null;
+    }
+
+    this._setTasksFetchingState(true);
+
+    const createdTask = await api.createTask(MAIN_URL, TOKEN, this.state.newTaskMessage);
+
+    this.setState(({ tasks }) => {
+      return { tasks: [...tasks, createdTask], newTaskMessage: '' };
+    });
+
+    this._setTasksFetchingState(false);
+  };
+
+  _updateTasksFilter = event => {
+    this.setState({ tasksFilter: event.target.value.toLowerCase() });
+  };
+
+  _updateNewTaskMessage = event => {
+    this.setState({ newTaskMessage: event.target.value });
+  };
+
+  _completeAllTasksAsync = async () => {
+    let notCompletedTasks = this.state.tasks.filter(task => task.completed !== true);
+    if (notCompletedTasks.length === 0) {
+      return null;
+    }
+
+    this._setTasksFetchingState(true);
+
+    notCompletedTasks = notCompletedTasks.map(item => {
+      return { ...item, completed: true };
+    });
+
+    api.completeAllTasks(MAIN_URL, TOKEN, notCompletedTasks);
+
+    this.setState(state => {
+      const tasks = state.tasks.map(item => {
+        return { ...item, completed: true };
+      });
+
+      return { tasks: this._sortTasks(tasks), allTasksChecked: true };
+    });
+
+    this._setTasksFetchingState(false);
+  };
+
+  _getAllCompleted = () => {
+    if (this.state.tasks.some(task => task.completed === false) === false) {
+      return true;
+    }
+    return false;
+  };
+
+  _sortTasks = tasks => {
+    let tasksCopy = JSON.parse(JSON.stringify(tasks));
+
+    tasksCopy.sort((a, b) => {
+      if (a.favorite > b.favorite) {
+        return -1;
+      }
+      if (a.favorite < b.favorite) {
+        return 1;
+      }
+      return 0;
+    });
+
+    tasksCopy.sort((a, b) => {
+      if (a.completed < b.completed) {
+        return -1;
+      }
+      if (a.completed > b.completed) {
+        return 1;
+      }
+      return 0;
+    });
+
+    return tasksCopy;
+  };
+
+  _fetchTasksAsync = async () => {
+    this._setTasksFetchingState(true);
+
+    const tasks = await api.fetchTasks(MAIN_URL, TOKEN);
+
+    this.setState({
+      tasks: this._sortTasks(tasks),
+    });
+
+    if (this._getAllCompleted()) {
+      this.setState({
+        allTasksChecked: true,
+      });
+    }
+
+    this._setTasksFetchingState(false);
+  };
+
+  _setTasksFetchingState = value => {
+    this.setState({
+      isTasksFetching: value,
+    });
+  };
+
+  render() {
+    const { tasks, isTasksFetching, allTasksChecked, tasksFilter, newTaskMessage } = this.state;
+    let taskJSX;
+
+    const taskCallback = task => {
+      return (
+        <Catcher key={task.id}>
+          <Task
+            {...task}
+            _removeTaskAsync={this._removeTaskAsync}
+            _updateTaskAsync={this._updateTaskAsync}
+          />
+        </Catcher>
+      );
+    };
+
+    if (!tasksFilter) {
+      taskJSX = tasks.map(taskCallback);
+    } else {
+      let filteredTasks = tasks.filter(task => task.message.includes(tasksFilter));
+
+      taskJSX = filteredTasks.map(taskCallback);
+    }
+
+    return (
+      <section className={Styles.scheduler}>
+        <Spinner isSpinning={isTasksFetching} />
+        <main>
+          <header>
+            <h1>Todo List</h1>
+            <input
+              placeholder='Поиск'
+              type='search'
+              value={tasksFilter}
+              onChange={this._updateTasksFilter}
+            />
+          </header>
+          <section>
+            <form onSubmit={this._createTaskAsync}>
+              <input
+                maxLength='50'
+                placeholder='Описaние моей новой задачи'
+                type='text'
+                onChange={this._updateNewTaskMessage}
+                value={newTaskMessage}
+              />
+              <button>Добавить задачу</button>
+            </form>
+            <ul>
+              <FlipMove>{taskJSX}</FlipMove>
+            </ul>
+          </section>
+          <footer>
+            <Checkbox
+              onClick={this._completeAllTasksAsync}
+              className={Styles.toggleTaskCompletedState}
+              color1='#363636'
+              color2='#FFF'
+              checked={allTasksChecked}
+              hover={false}
+            />
+            <span className={Styles.completeAllTasks}>Все задачи выполнены</span>
+          </footer>
+        </main>
+      </section>
+    );
+  }
 }
--- source/components/Task/index.js
+++ source/components/Task/index.js
@@ -8,169 +8,176 @@ import { string, bool } from 'prop-types';
 import cx from 'classnames';
 
 export default class Task extends PureComponent {
-    static propTypes = {
-        id: string.isRequired,
-        completed: bool.isRequired,
-        favorite: bool.isRequired,
-        message: string.isRequired,
-        created: string.isRequired,
-    };
+  static propTypes = {
+    id: string.isRequired,
+    completed: bool.isRequired,
+    favorite: bool.isRequired,
+    message: string.isRequired,
+    created: string.isRequired,
+  };
 
-    state = { isTaskEditing: false, newTaskMessage: this.props.message };
+  state = { isTaskEditing: false, newTaskMessage: this.props.message };
 
-    taskInput = React.createRef();
+  taskInput = React.createRef();
 
-    _getTaskShape = ({
-        id = this.props.id,
-        completed = this.props.completed,
-        favorite = this.props.favorite,
-        message = this.props.message,
-    }) => ({
-        id,
-        completed,
-        favorite,
-        message,
+  _getTaskShape = ({
+    id = this.props.id,
+    completed = this.props.completed,
+    favorite = this.props.favorite,
+    message = this.props.message,
+  }) => ({
+    id,
+    completed,
+    favorite,
+    message,
+  });
+
+  _toggleTaskFavoriteState = () => {
+    const task = this._getTaskShape(this.props);
+
+    this.props._updateTaskAsync({ ...task, favorite: !task.favorite });
+  };
+
+  _setTaskEditingState = editingState => {
+    if (editingState) {
+      this.setState(
+        {
+          isTaskEditing: true,
+        },
+        () => {
+          if (this.state.isTaskEditing) {
+            this.taskInput.current.focus();
+          }
+        }
+      );
+
+      this.taskInput.current.focus();
+    } else {
+      this.setState(() => ({
+        isTaskEditing: false,
+      }));
+    }
+  };
+
+  _updateNewTaskMessage = event => {
+    this.setState({
+      newTaskMessage: event.target.value,
+    });
+  };
+
+  _updateTask = () => {
+    if (this.props.message === this.state.newTaskMessage) {
+      this._setTaskEditingState(false);
+
+      return null;
+    }
+
+    const task = this._getTaskShape(this.props);
+
+    this.props._updateTaskAsync({ ...task, message: this.state.newTaskMessage });
+
+    this._setTaskEditingState(false);
+  };
+
+  _updateTaskMessageOnClick = () => {
+    if (!this.state.newTaskMessage) {
+      return null;
+    }
+
+    if (this.state.isTaskEditing === true) {
+      this._updateTask();
+      return null;
+    } else {
+      this._setTaskEditingState(true);
+    }
+  };
+
+  _updateTaskMessageOnKeyDown = event => {
+    if (this.state.newTaskMessage === '') {
+      return null;
+    }
+
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      this._updateTask();
+    } else if (event.key === 'Escape') {
+      this._cancelUpdatingTaskMessage();
+    }
+  };
+
+  _cancelUpdatingTaskMessage = () => {
+    this.setState({
+      newTaskMessage: this.props.message,
+      isTaskEditing: false,
+    });
+  };
+
+  _toggleTaskCompletedState = () => {
+    let task = this._getTaskShape(this.props);
+
+    this.props._updateTaskAsync({ ...task, completed: !task.completed });
+  };
+
+  _removeTask = () => {
+    this.props._removeTaskAsync(this.props.id);
+  };
+
+  render() {
+    const { completed } = this.props;
+    const { newTaskMessage, isTaskEditing } = this.state;
+
+    const statusStyle = cx(Styles.task, {
+      [Styles.completed]: completed,
     });
 
-    _toggleTaskFavoriteState = () => {
-        const task = this._getTaskShape(this.props);
-
-        this.props._updateTaskAsync({ ...task, favorite: !task.favorite });
-    };
-
-    _setTaskEditingState = editingState => {
-        if (editingState) {
-            this.setState({
-                isTaskEditing: true,
-            });
-
-            setImmediate(() => this.taskInput.current.focus());
-        } else {
-            this.setState(() => ({
-                isTaskEditing: false,
-            }));
-        }
-    };
-
-    _updateNewTaskMessage = event => {
-        this.setState({
-            newTaskMessage: event.target.value,
-        });
-    };
-
-    _updateTask = () => {
-        if (this.props.message === this.state.newTaskMessage) {
-            this._setTaskEditingState(false);
-
-            return null;
-        }
-
-        const task = this._getTaskShape(this.props);
-
-        this.props._updateTaskAsync({ ...task, message: this.state.newTaskMessage });
-
-        this._setTaskEditingState(false);
-    };
-
-    _updateTaskMessageOnClick = () => {
-        if (this.state.newTaskMessage === '') {
-            return null;
-        }
-
-        if (this.state.isTaskEditing === true) {
-            this._updateTask();
-            return null;
-        } else {
-            this._setTaskEditingState(true);
-        }
-    };
-
-    _updateTaskMessageOnKeyDown = event => {
-        if (this.state.newTaskMessage === '') {
-            return null;
-        }
-
-        if (event.key === 'Enter') {
-            event.preventDefault();
-            this._updateTask();
-        } else if (event.key === 'Escape') {
-            this._cancelUpdatingTaskMessage();
-        }
-    };
-
-    _cancelUpdatingTaskMessage = () => {
-        this.setState({
-            newTaskMessage: this.props.message,
-            isTaskEditing: false,
-        });
-    };
-
-    _toggleTaskCompletedState = () => {
-        let task = this._getTaskShape(this.props);
-
-        this.props._updateTaskAsync({ ...task, completed: !task.completed });
-    };
-
-    _removeTask = () => {
-        this.props._removeTaskAsync(this.props.id);
-    };
-
-    render() {
-        const { completed } = this.props;
-        const { newTaskMessage, isTaskEditing } = this.state;
-
-        const statusStyle = cx(Styles.task, {
-            [Styles.completed]: completed,
-        });
-
-        return (
-            <li className={statusStyle}>
-                <div className={Styles.content}>
-                    <Checkbox
-                        onClick={this._toggleTaskCompletedState}
-                        className={Styles.toggleTaskCompletedState}
-                        inlineBlock={true}
-                        color1='#3B8EF3'
-                        color2='#FFF'
-                        checked={completed}
-                        hover={false}
-                    />
-                    <input
-                        disabled={!isTaskEditing}
-                        maxLength='50'
-                        type='text'
-                        value={newTaskMessage}
-                        ref={this.taskInput}
-                        onChange={this._updateNewTaskMessage}
-                        onKeyDown={this._updateTaskMessageOnKeyDown}
-                    />
-                </div>
-                <div className={Styles.actions}>
-                    <Star
-                        className={Styles.toggleTaskFavoriteState}
-                        inlineBlock={true}
-                        color1='#3B8EF3'
-                        color2='#000'
-                        onClick={this._toggleTaskFavoriteState}
-                        checked={this.props.favorite}
-                    />
-                    <Edit
-                        className={Styles.updateTaskMessageOnClick}
-                        inlineBlock={true}
-                        color1='#3B8EF3'
-                        color2='#000'
-                        onClick={this._updateTaskMessageOnClick}
-                        checked={this.state.isTaskEditing}
-                    />
-                    <Remove
-                        onClick={this._removeTask}
-                        className={Styles.cross}
-                        inlineBlock={true}
-                        color1='#3B8EF3'
-                        color2='#000'
-                    />
-                </div>
-            </li>
-        );
-    }
+    return (
+      <li className={statusStyle}>
+        <div className={Styles.content}>
+          <Checkbox
+            onClick={this._toggleTaskCompletedState}
+            className={Styles.toggleTaskCompletedState}
+            inlineBlock={true}
+            color1='#3B8EF3'
+            color2='#FFF'
+            checked={completed}
+            hover={false}
+          />
+          <input
+            disabled={!isTaskEditing}
+            maxLength='50'
+            type='text'
+            value={newTaskMessage}
+            ref={this.taskInput}
+            onChange={this._updateNewTaskMessage}
+            onKeyDown={this._updateTaskMessageOnKeyDown}
+          />
+        </div>
+        <div className={Styles.actions}>
+          <Star
+            className={Styles.toggleTaskFavoriteState}
+            inlineBlock={true}
+            color1='#3B8EF3'
+            color2='#000'
+            onClick={this._toggleTaskFavoriteState}
+            checked={this.props.favorite}
+          />
+          <Edit
+            className={Styles.updateTaskMessageOnClick}
+            inlineBlock={true}
+            color1='#3B8EF3'
+            color2='#000'
+            onClick={this._updateTaskMessageOnClick}
+            checked={this.state.isTaskEditing}
+          />
+          <Remove
+            onClick={this._removeTask}
+            className={Styles.cross}
+            inlineBlock={true}
+            color1='#3B8EF3'
+            color2='#000'
+          />
+        </div>
+      </li>
+    );
+  }
 }
